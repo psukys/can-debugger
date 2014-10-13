15ae184d19b2fb266d5b606c540e09c0ccd5bf05
Paulius Šukys <paul.sukys@gmail.com>
2014-10-13 15:31:03 +0300

    Restructured into a simple data->node relation

--- src/data-structure.py
+++ src/data-structure.py
@@ -1,27 +1,48 @@
 from datetime import datetime
 
-class CANNode:
+
+class CANData:
+
     """The most simplest data structure
     which is self implied by CAN dump
     """
+
     def __init__(self, time=None, addr=None, data=None):
         """Initializer
         Args:
             time: time at which the data dump was taken
+            data: specific data sent at that point
         """
         if time:
             self.time = time
 
-        if addr:
-            self.addr = addr
-
         if data:
             self.data = data
 
-    def parse_line(self, line):
+    def __str__(self):
+        return "{0} {2}".format(self.time,
+                                self.data)
+
+
+class CANNode:
+
+    """A little more advanced instance
+    which groups data to addresses"""
+
+    def __init__(self, addr):
+        """Initializer
+        Args:
+            addr: the address representable in a string
+        """
+        self.addr = addr
+        self.data = {}
+
+    def add_data(self, line):
         """Parses a string filled with data
         in such format:
-        (123456.789) interface1 0C94#001A
+
+          (123456.789) interface1 0C94#001A
+
         The number in clauses is unix timestamp
         interface1 - interface name
         hex number before hash (#) - address
@@ -30,17 +51,15 @@ class CANNode:
         Args:
             line: string filled with data
         """
-        m = re.match("\((\d+\.\d+)\) (\S+) ([0-9a-fA-F]+)#([0-9a-fA-F]+)", line)
-        #first match group: time
-        self.time = datetime.utcfromtimestamp(float(m.group(1)))
-        #second match group is not needed yet,
-        #but leaving it for later implementation
-        self.addr = m.group(3).strip()
-        self.data = m.group(4).strip()
+        m = re.match(
+            "\((\d+\.\d+)\) (\S+) ([0-9a-fA-F]+)#([0-9a-fA-F]+)", line)
+
+        if m.group(3).strip() == self.addr:
+            self.data.update({utcfromtimestamp(float(m.group(1))):
+                              m.group(4).strip()})
+        else:
+            raise("Wrong address CAN address found in parsed line")
 
     def __str__(self):
-        return "{0} {1}:{2}".format(self.time,
-                                    self.addr,
-                                    self.data)
-
-class 
+        return "Address: {0}, data points: {1}".format(self.addr,
+                                                       len(self.data))
