ea80cce30044319e64713e09da060011821b5985
Paulius Šukys <paul.sukys@gmail.com>
2014-10-14 15:56:08 +0300

    Fixed assertion logic to checking whether the target value is available in source test case

--- src/data_diff.py
+++ src/data_diff.py
@@ -2,8 +2,10 @@ from data_structure import CANNode, CANTestCase
 
 
 class CANDiff:
+
     """A class for finding differences between testcase
     """
+
     def __init__(self):
         pass
 
@@ -17,18 +19,25 @@ class CANDiff:
             (or the address do not exist)
         """
         addrs = []
-        #check the test case that is being compared
+        # check the test case that is being compared
         for node in trg.nodes:
-            #check if address exists in source
+            # check if address exists in source
             src_addr = src.get_index_by_addr(node.addr)
             if src_addr is not None:
-                #linear check for data difference 
+                # linear check for data difference
                 for idx, time in enumerate(node.data):
-                    #check if there's any difference between data points
-                    if not any(src.nodes[src_addr].data[t] == node.data[time]
-                               for t in src.nodes[src_addr].data):
+                    # check if there's any difference between data points
+                    if node.data[time] not in src.nodes[src_addr].data.values():
                         addrs.append(node.addr)
                         break
             else:
                 addrs.append(node.addr)
         return addrs
+
+    def get_diff_data(self, src, trg):
+        data = []
+        addrs = self.get_diff_addrs(src, trg)
+        for addr in addrs:
+            idx = src.get_index_by_addr(addr)
+            if idx is not None:
+                pass
